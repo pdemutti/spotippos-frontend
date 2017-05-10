bb3e05d0dcc286268a32b248baf438830ab4d754
Paulo Demutti <paulodemutti@gmail.com>
2017-05-10 10:07:27 -0300

    filter params

--- app/js/main.js
+++ app/js/main.js
@@ -3,16 +3,74 @@ var App = App || {};
 App.Spotippos = (function ($, win, doc) {
   'use strict';
 
+    var params = {}; // [{ax: 1}, {page: 4}, {bx: 20}]
+
     function setup () {
         // var domainApi = '/proxy';
         // getData(domainApi + "/properties?beds=1&maxprice=420000&page=3", function(data){
         //     var json = JSON.parse(data);
         //     createHtml(json.properties);
         // });
+        addEvent(doc.getElementById('ax'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('ay'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('bx'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('by'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('id'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('squareMeters'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('beds'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('baths'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('minprice'), 'change', function change() {
+          filter();
+        });
+
+        addEvent(doc.getElementById('maxprice'), 'change', function change() {
+          filter();
+        });
     }
+
+    function getParams() {
+      var urlEncondedParams = '';
+      var i;
+      var prop;
+      var date = new Date();
+
+      for (prop in params) {
+        urlEncondedParams += prop + "=" + params[prop] + "&";
+      }
+
+      urlEncondedParams += '_' + date.getTime();
+
+      // retornará: ax=1&page=4&bx=20
+      return urlEncondedParams;
+    }
+
     function getData (url, success){
       var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-      xhr.open('GET', url);
+      xhr.open('GET', url + "?" + getParams());
       xhr.onreadystatechange = function() {
           if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
       };
@@ -20,14 +78,40 @@ App.Spotippos = (function ($, win, doc) {
       xhr.send();
       return xhr;
     }
-    function createHtml (data){
- 
+    function success (data){
+      console.log(data);
     }
     function addEvent (el, type, handler){
       if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
     }
     function removeEvent (el, type, handler){
       if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
+    }
+
+    function filter () {
+      var form = doc.getElementById('formFilter');
+      var inputs = form.getElementsByTagName('input');
+      var i;
+      var input;
+
+      for (i = 0; i < inputs.length; i ++) {
+        input = inputs[i];
+
+        // {ax: 1}
+        if (input.value !== '') {
+          params[input.getAttribute('name')] = input.value;
+        } else {
+          delete params[input.getAttribute('name')];
+        }
+      }
+
+      getData('/filter', success);
+    }
+
+    function paginate(pageNumber) {
+      params.page = pageNumber;
+
+      getData('/filter', success);
     }
 
     return {
--- index.js
+++ index.js
@@ -13,31 +13,51 @@ app.set('view engine', 'hbs')
 
 var users = []
 
-fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-  if (err) throw err
-
-  JSON.parse(data).forEach(function (user) {
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    users.push(user)
-  })
-
-})
 app.use('/proxy', proxy('http://spotippos.vivareal.com', {
   filter: function(req, res) {
      return req.method == 'GET';
   }
 }));
 app.get('/', function (req, res) {
+  console.log(req);
   // res.render('index', {users: users})
   axios.get('http://spotippos.vivareal.com/properties?ax=1&ay=1&bx=20&by=20')
   .then(function (response) {
-    res.render('index', {room: response})
-    console.log(response);
+    res.render('index', {rooms: response.data.properties})
+    // console.log(response.data.properties);
   })
   .catch(function (error) {
     console.log(error);
   });
 })
+
+app.get('/filter', function (req, res) {
+  console.log(req.query, getParams(req.query));
+  axios.get('http://spotippos.vivareal.com/properties?' + getParams(req.query))
+  .then(function (response) {
+     console.log(response.data);
+    res.json(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+});
+
+function getParams(params) {
+  var urlEncondedParams = '';
+  var i;
+  var prop;
+  var date = new Date();
+
+  for (prop in params) {
+    urlEncondedParams += prop + "=" + params[prop] + "&";
+  }
+
+  urlEncondedParams += '_' + date.getTime();
+
+  // retornará: ax=1&page=4&bx=20
+  return urlEncondedParams;
+}
 
 app.use('/static', express.static(__dirname + '/public'));
 
